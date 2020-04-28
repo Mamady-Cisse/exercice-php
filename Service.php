7f66b45166a9a633ccc1ff62ab2f070586af114c
Cissé <64393632+Mamady-Cisse@users.noreply.github.com>
2020-04-28 04:23:38 +0000

    Add files via upload

---
+++ Service.php
@@ -0,0 +1,15 @@
+<?php
+class Service{
+    private $Service1;
+    private $Service2;
+
+    public function __construct(){
+    
+    }
+
+public function getService1(){
+    return $this->Service1;
+}
+
+public function getService2(){
+    return $this->Service2;
---
+++ classe,sex.php
@@ -0,0 +1,24 @@
+<?php
+class Sexe{
+    private $masculin;
+    private $feminin;
+
+    public function __construct(){
+    
+    }
+
+public function getmasculin(){
+    return $this->masculin;
+}
+
+public function getfeminin(){
+    return $this->feminin;
+}
+public function setmasculin($masculin){
+    $this->masculin=$masculin;
+}
+
+
+
+}
+?>
